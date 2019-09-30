0617a540d7b441a7ed3d6979194ffade25237b9e
SAKI-toki <toki.ryuka.senriyama@gmail.com>
2019-09-30 10:08:34 +0900

    Implement PlayerController

---
+++ ProjectData/Assets/Scripts/Player/FlashLightController.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// ジャイロの回転を取得
+/// </summary>
+public class FlashLightController : MonoBehaviour
+{
+    [SerializeField]
+    Vector3 rotLimitAbs = new Vector3();
+    void Update()
+    {
+        var prevRotation = transform.localRotation;
+        //コントローラーの向きと同じ方向に回転する
+        transform.localRotation = SwitchGyro.GetGyro(0);
+        var eulerAngles = transform.localEulerAngles;
+        //X軸回転の制御
+        if (eulerAngles.x > rotLimitAbs.x &&
+        eulerAngles.x < 360 - rotLimitAbs.x)
+        {
+            eulerAngles.x = (eulerAngles.x < 180 ? 1 : -1) * rotLimitAbs.x;
+        }
+        //Y軸回転の制御
+        if (eulerAngles.y > rotLimitAbs.y &&
+        eulerAngles.y < 360 - rotLimitAbs.y)
+        {
+            eulerAngles.y = (eulerAngles.y < 180 ? 1 : -1) * rotLimitAbs.y;
+        }
+        //Z軸回転の制御
+        if (eulerAngles.z > rotLimitAbs.z &&
+        eulerAngles.z < 360 - rotLimitAbs.z)
+        {
+            eulerAngles.z = (eulerAngles.z < 180 ? 1 : -1) * rotLimitAbs.z;
+        }
+        transform.localEulerAngles = eulerAngles;
+        //前を向くようにリセット
+        if (SwitchInput.GetButtonDown(0, SwitchButton.Up))
+        {
+            SwitchGyro.SetBaseGyro(0);
+        }
+    }
+}
---
+++ ProjectData/Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    float moveSpeed = 1.0f;
+    [SerializeField]
+    float rotateSpeed = 1.0f;
+
+    void Start()
+    {
+        //地面に落としておく
+        Physics.autoSimulation = false;
+        Physics.Simulate(10.0f);
+        Physics.autoSimulation = true;
+    }
+
+    void Update()
+    {
+        float moveStick = SwitchInput.GetVertical(0);
+        float rotateStick = SwitchInput.GetHorizontal(0);
+
+        transform.Translate(Vector3.forward * moveStick * moveSpeed * Time.deltaTime);
+        transform.Rotate(0, rotateStick * rotateSpeed * Time.deltaTime, 0);
+    }
+}
